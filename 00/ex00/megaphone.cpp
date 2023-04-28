e310e7a30fd53d0272d97749fa89775c92efb7f1
mariekart42 <schwokelbaer@gmail.com>
2023-04-28 13:15:06 +0200

    first exec

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -4,5 +4,19 @@
 int main(int argc, char **argv)
 {
 	if (argc < 2)
-		std::cout << * LOUD AND UNBEARABLE FEEDBACK NOISE *
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i=1; i < argc; i++)
+		{
+			for (int k=0; argv[i][k]; k++)
+			{
+				if (argv[i][k] >= 97 && argv[i][k] <= 122)
+					std::cout << (char)(argv[i][k] - 32);
+				else
+					std::cout << (argv[i][k]);
+			}
+		}
+		std::cout << "\n";
+	}
 }
