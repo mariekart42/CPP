989f0b453190f1c1f49c190d46e95784a066191e
mariekart42 <schwokelbaer@gmail.com>
2023-05-04 20:01:32 +0200

    something lol

--- 00/ex01/phoneBook.cpp
+++ 00/ex01/phoneBook.cpp
@@ -5,11 +5,23 @@
 int main(void)
 {
 	phoneBook myPhoneBook;
-	contact myContact;
+	contact contactArray[8];
+
+std::string str;
 
 	myPhoneBook.name = "Yoshi";
 	myPhoneBook.name = "Yoshi 2";
 	std::cout << myPhoneBook.name << std::endl;
-	myContact.name = "Yeee";
-	std::cout << contact.name << std::endl;
+	contactArray[0].name = "contact uno";
+	contactArray[1].name = "contact dos";
+	contactArray[2].name = "contact tres";
+	// str = contactArray[0];
+	// contactArray[1] = "contact dos";
+	// contactArray[2] = "contact tres";
+	std::cout << contactArray[0].name << std::endl;
+	std::cout << contactArray[1].name << std::endl;
+	std::cout << contactArray[2].name << std::endl;
+	// std::cout << contactArray[1] << std::endl;
+	// std::cout << contactArray[2] << std::endl;
+
 }
